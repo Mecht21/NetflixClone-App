dec8a4e272fb9f10b0d4b5c9174d60c57c45c7c0
Daniel <daniel.serrano0119@gmail.com>
2023-12-21 17:27:20 -0600

    We finished with the Auth Screen UI.

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,30 @@
+/**
+ * Input Component
+ *
+ * This component represents a reusable input field with a label for forms.
+ *
+ * @component
+ * @example
+ * import Input from "@/components/Input";
+ *
+ * // Example usage in TSX
+ * <Input
+ *   id="username"
+ *   onChange={(event) => handleUsernameChange(event)}
+ *   value={username}
+ *   label="Username"
+ *   type="text"
+ * />
+ */
 import React from 'react';
+
+/**
+ * InputProps Interface
+ *
+ * Defines the prop types for the Input component.
+ *
+ * @interface InputProps
+ */
 
 interface InputProps {
   id: string;
@@ -7,6 +33,14 @@ interface InputProps {
   label: string;
   type?: string;
 }
+
+/**
+ * Input Component
+ *
+ * @function Input
+ * @param {InputProps} props - The properties passed to the Input component.
+ * @returns {JSX.Element} - The rendered Input component.
+ */
 
 const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
@@ -57,4 +91,5 @@ const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   )
 }
 
+// Export the Input component as the default export
 export default Input;
